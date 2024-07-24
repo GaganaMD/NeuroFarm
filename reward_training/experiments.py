67d0b186616397839a3384b34f98ff76cf6c2cf8
Maria <mariarosekesa@gmail.com>
2024-07-24 18:34:10 +0100

    works

---
+++ reward_training/experiments.py
@@ -0,0 +1,93 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from neuromatch2024_experiments.q_networks import RNNQNetwork, LSTMQNetwork, GRUQNetwork
+from neuromatch2024_experiments.agents import DQNAgent
+import torch.nn as nn
+from train import vanilla_train, mnist_train, cifar_train
+
+configurations = [
+    ("short-seq", "easy-seq", "RNN", "Rew-1"),
+    ("short-seq", "easy-seq", "RNN", "Rew-10"),
+    ("long-seq", "cifar10-seq", "LSTM", "Rew-10"),
+    ("long-seq", "cifar10-seq", "LSTM", "Rew-100")
+]
+
+networks = {
+    "RNN": RNNQNetwork,
+    "LSTM": LSTMQNetwork,
+    # "GRU": GRUQNetwork
+}
+
+state_sizes = {
+    "short-seq": 6,
+    "long-seq": 11
+}
+
+experiments = {}
+
+for item in configurations:
+    experiments[item] = {}
+    if item[1] == 'easy-seq':
+        state_size = state_sizes[item[0]]
+        action_size = state_size
+    elif item[1] == 'cifar10-seq':
+        # Logits for MNIST and CIFAR10 are 10-dimensional
+        state_size = 10
+        if item[0] == 'short-seq':
+            action_size = 6
+        elif item[0] == 'long-seq':
+            action_size = 11
+    # if item[0] == "easy-seq":
+        # state_size = 6
+        # action_size = state_size
+    # elif item[0] == 'cifar10-seq':
+        # Logits for MNIST and CIFAR10 are 10-dimensional
+        # state_size = 10
+        # if seq_type == 'short-seq':
+            # action_size = 6
+        # elif seq_type == 'long-seq':
+            # action_size = 11
+    # if item[0] == "short-seq":
+        # experiments[item]['state_size'] = 6
+    # elif item[0] == "long-seq":
+        # experiments[item]['state_size'] = 11
+    if item[1] == "easy-seq":
+        experiments[item]['train_function'] = vanilla_train
+    else:
+        experiments[item]['train_function'] = cifar_train
+    if item[2] == "RNN":
+        experiments[item]['q_network'] = RNNQNetwork
+    elif item[2] == "LSTM":
+        experiments[item]['q_network'] = LSTMQNetwork
+    if item[3] == "Rew-1":
+        experiments[item]['reward'] = 1
+        rew = 1
+    elif item[3] == "Rew-10":
+        experiments[item]['reward'] = 10
+        rew = 10
+    elif item[3] == "Rew-100":
+        experiments[item]['reward'] = 100
+        rew = 100
+    hidden_size = 64 if item[0] == "short-seq" else 128
+
+    model_path = f"models/{item[0]}_{item[1]}_MSELoss_{item[2]}_rew-{rew}.pth"
+    learning_curve = f"models/{item[0]}_{item[1]}_MSELoss_{item[2]}_rew-{rew}.npy"
+
+    experiments[item]['hidden_size'] = hidden_size
+
+    experiments[item]['loss'] = nn.MSELoss
+    experiments[item]['action_size'] = action_size
+    experiments[item]['state_size'] = state_size
+    experiments[item]['capacity'] = 100000
+    experiments[item]['batch_size'] = 32
+    experiments[item]['lr'] = 0.001 if item[0] == "short-seq" else 0.0001
+    experiments[item]['gamma'] = 0.99
+    experiments[item]['model_path'] = model_path
+    experiments[item]['mode'] = 'train-from-zero'
+    experiments[item]['train_steps'] = 50000
+
+    # if item == ("long-seq", "cifar10-seq", "LSTM", "Rew-100"):
+    # experiments[item]['hidden_size'] = 512
---
+++ reward_training/train.py
@@ -0,0 +1,164 @@
+from environments import DelaySampleToMatchEnv
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from neuromatch2024_experiments.q_networks import RNNQNetwork, LSTMQNetwork, GRUQNetwork
+from neuromatch2024_experiments.agents import DQNAgent
+import numpy as np
+import torch
+import torch.nn.functional as F
+import time
+
+
+def move_tuple_to_device(hidden_tuple, device):
+    return (hidden_tuple[0].to(device), hidden_tuple[1].to(device))
+
+
+def vanilla_train(config_dict):
+    start = time.time()
+    n_stimuli = config_dict['state_size'] - 1
+    reward = config_dict['reward']
+
+    env = DelaySampleToMatchEnv(reward=reward, n_stimuli=n_stimuli)
+    if config_dict['q_network'] == RNNQNetwork:
+        agent = DQNAgent(config_dict)
+    elif config_dict['q_network'] == LSTMQNetwork:
+        agent = LSTMDQNAgent(config_dict)
+
+    n_episodes = config_dict['train_steps']
+    episode_rewards = []
+    scores = []
+
+    # Training loop
+    for i in range(n_episodes):
+        state = env.reset()  # Reset the environment
+        state = F.one_hot(torch.tensor(state),
+                          env.observation_space.n).to(dtype=torch.float32, device=agent.device)
+        done = False
+        score = 0
+        hidden = agent.q_network.init_hidden()
+        if isinstance(hidden, tuple):
+            hidden = move_tuple_to_device(hidden, agent.device)
+        else:
+            hidden = hidden.to(agent.device)
+
+        while not done:
+            action, next_hidden = agent.select_action(state, hidden)
+            next_state, reward, done, info = env.step(
+                action)  # Take the action
+            next_state = F.one_hot(torch.tensor(next_state),
+                                   env.observation_space.n).to(dtype=torch.float32, device=agent.device)
+            # ('state', 'action', 'next_state', 'reward', 'hidden', 'next_hidden', 'done'))
+            agent.store_transition(state, action, next_state,
+                                   reward, hidden, next_hidden, done)
+            agent.learn()  # Update Q-network
+            hidden = next_hidden
+            state = next_state  # Move to the next state
+            score += reward
+
+        scores.append(score)
+
+        if i % 100 == 0:
+            avg_score = np.mean(scores[-100:])
+            episode_rewards.append(avg_score)
+            print(f"Episode {i} - Average Score: {avg_score:.2f}")
+
+    end = time.time()
+    agent.save_model()
+    print(f'It took {end-start} seconds to train the model')
+    np.save(config_dict['learning_curve_path'], episode_rewards)
+
+
+def cifar_train(config_dict):
+    def prepare_data():
+        train_data = np.load(
+            # '../cifar10_task/logits/embeddings copy.npy')
+            '../cifar10_task/logits/logits_cifar10.npy')
+        train_labels = np.load(
+            # '../cifar10_task/logits/labels copy.npy')
+            '../cifar10_task/logits/labels_cifar10.npy')
+
+        # Adjust train_labels as per the original intent (adding 1)
+        train_labels = train_labels + 1
+
+        # Create a row of zeros with the same number of columns as train_data
+        zeros_row = np.zeros((1, train_data.shape[1],))
+        # print(train_data.shape, zeros_row.shape)
+        train_data = torch.tensor(
+            np.vstack((zeros_row, train_data)), dtype=torch.float32).to(device='cuda')
+
+        # Append the label corresponding to the zeros row
+        train_labels = np.hstack((0, train_labels))
+
+        # print("Shape of train_labels_with_zeros:", train_labels.shape)
+        # print("Shape of train_data_with_zeros:", train_data.shape)
+
+        # Create a dictionary to store indices of each class
+        class_dct = {}
+        for i in range(11):  # Adjusted to iterate from 0 to 10 (inclusive)
+            class_dct[i] = np.where(train_labels == i)[0]
+
+        # Print example usage of class_dct
+        # print("Indices of class 0:", class_dct[0])
+
+        return train_data, class_dct
+
+    train_data, class_dct = prepare_data()
+
+    start = time.time()
+    # n_stimuli = config_dict['action_size'] - 1
+    # print('boom!', n_stimuli)
+    n_stimuli = config_dict['action_size'] - 1
+    env = DelaySampleToMatchEnv(reward=reward, n_stimuli=n_stimuli)
+    if config_dict['q_network'] == RNNQNetwork:
+        agent = DQNAgent(config_dict)
+    elif config_dict['q_network'] == LSTMQNetwork:
+        agent = LSTMDQNAgent(config_dict)
+
+    n_episodes = config_dict['train_steps']
+    episode_rewards = []
+    scores = []
+
+    for i in range(n_episodes):
+        done = False
+        state = env.reset()  # Reset the environment
+        indices = class_dct[int(state)]
+        random_index = np.random.choice(indices)
+        state = train_data[random_index].flatten()
+        score = 0
+        hidden = agent.q_network.init_hidden()
+        if isinstance(hidden, tuple):
+            hidden = move_tuple_to_device(hidden, agent.device)
+        else:
+            hidden = hidden.to(agent.device)
+        while not done:
+            action, next_hidden = agent.select_action(state, hidden)
+            next_state, reward, done, info = env.step(
+                action)  # Take the action
+            indices = class_dct[int(next_state)]
+            random_index = np.random.choice(indices)
+            next_state = train_data[random_index].flatten()
+            # ('state', 'action', 'next_state', 'reward', 'hidden', 'next_hidden', 'done'))
+            agent.store_transition(state, action, next_state,
+                                   reward, hidden, next_hidden, done)
+            agent.learn()  # Update Q-network
+            hidden = next_hidden
+            state = next_state  # Move to the next state
+            score += reward
+        scores.append(score)
+
+        if i % 100 == 0:
+            avg_score = np.mean(scores[-100:])
+            episode_rewards.append(avg_score)
+            print(f"Episode {i} - Average Score: {avg_score:.2f}")
+
+    end = time.time()
+    agent.save_model()
+    print(f'It took {end-start} seconds to train the model')
+    np.save(config_dict['learning_curve_path'], episode_rewards)
+
+
+def mnist_train(config_dict):
+    pass

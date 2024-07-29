3d7ca8f48b553cac4f91a31489740086ca492a8e
Maria <mariarosekesa@gmail.com>
2024-07-29 15:45:29 +0100

    chatgpt creation

--- data_analysis/test.py
+++ data_analysis/test.py
@@ -7,7 +7,10 @@ dat = np.load(
     '/home/maria/NeuroFarm/learning_curves2/short-seq_easy-seq_MSELoss_RNN.npy')
 dat = np.load(
     '/home/maria/NeuroFarm/learning_curves2/short-seq_cifar10-seq_MSELoss_RNN.npy')
+dat = np.load(
+    '/home/maria/NeuroFarm/learning_curves2/long-seq_digits-seq_MSELoss_RNN.npy')
 print(dat.shape)
+
 rng = np.arange(0, dat.shape[0]) * 100
 plt.plot(rng, dat)
 plt.show()
